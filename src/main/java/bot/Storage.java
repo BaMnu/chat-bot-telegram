f1973b4c1ec0ff292dcfa01ad02d11dd39c86862
KosNet75 <nemokonstantin@gmail.com>
2022-08-23 00:49:00 +0700

    first

--- src/main/java/bot/Storage.java
+++ src/main/java/bot/Storage.java
@@ -1,4 +1,16 @@
 package bot;
 
 public class Storage {
+
+
+
+
+    rndComment();
+
+
+  private static void rndComment() {
+    List<String> result = Files.readAllLines(Paths.get(".\\src\\predictions.txt"));
+
+    System.out.println(result.get(ThreadLocalRandom.current().nextInt(0, result.size()-1)));
+  }
 }
