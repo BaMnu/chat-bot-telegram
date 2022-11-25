587f3d27822cee26ca962c040b2b7f6415fab8a8
Olga Fedorova <109962741+BaMnu@users.noreply.github.com>
2022-11-25 19:37:58 +0300

    Update Storage.java

--- src/main/java/bot/Storage.java
+++ src/main/java/bot/Storage.java
@@ -1,4 +1,17 @@
 package bot;
 
+import java.io.*;
+import java.util.*;
+
 public class Storage {
+    private final List<String> lines = new ArrayList<>();
+    protected String randomLine() throws IOException {
+        try (BufferedReader abc = new BufferedReader(new FileReader("predictions.csv"))) {
+            while (abc.readLine() != null) {
+                String line = abc.readLine();
+                lines.add(line);
+            }
+        }
+        return lines.get(new Random().nextInt(lines.size()));
+    }
 }
